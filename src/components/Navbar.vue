bdb9a9ff1f0b01371468a9c932fbba1fac269883
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-05-02 21:29:20 +0800

    Make Active navlink if one the page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,24 @@
   <nav
     class="flex justify-center items-center gap-x-6 p-4 bg-blue-400 text-white font-semibold"
   >
-    <RouterLink to="/">Todo</RouterLink>
-    <RouterLink to="/done-task">Done Task</RouterLink>
-    <RouterLink to="/available-task">Available Task</RouterLink>
+    <RouterLink
+      to="/"
+      :class="{ underline: $route.path === '/' }"
+      class="hover:underline"
+      >Todo</RouterLink
+    >
+    <RouterLink
+      to="/done"
+      :class="{ underline: $route.path === '/done' }"
+      class="hover:underline"
+      >Done Task</RouterLink
+    >
+    <RouterLink
+      to="/available"
+      :class="{ underline: $route.path === '/available' }"
+      class="hover:underline"
+      >Available Task</RouterLink
+    >
   </nav>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'todo',
-      component: HomeView
+      path: "/",
+      name: "todo",
+      component: HomeView,
     },
     {
-      path: '/done-task',
-      name: 'done-task',
-      component: () => import('../views/DoneView.vue')
+      path: "/done",
+      name: "done",
+      component: () => import("../views/DoneView.vue"),
     },
     {
-      path: '/available-task',
-      name: 'available-task',
-      component: () => import('../views/AvailableView.vue')
-    }
-  ]
-})
+      path: "/available",
+      name: "available",
+      component: () => import("../views/AvailableView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
